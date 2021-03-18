636193d701126c7dd2407853063d71f6c4e23dca
Sam Singer <sam.singet79@gmail.com>
2021-03-18 16:20:27 -0700

    done with it all

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,6 +1,6 @@
-import React from 'react';
-
-const Total = props => {
+import React from "react";
+import { connect } from "react-redux";
+const Total = (props) => {
   return (
     <div className="content">
       <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
@@ -8,4 +8,7 @@ const Total = props => {
   );
 };
 
-export default Total;
+const mapStateToProps = (state) => ({
+  count: state.car.price,
+});
+export default connect(mapStateToProps)(Total);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import "bulma/css/bulma.css";
+import "./styles.scss";
+import { createStore, applyMiddleware } from "redux";
+import { reducer } from "./reducers";
+import { Provider } from "react-redux";
+const store = createStore(reducer, applyMiddleware());
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
