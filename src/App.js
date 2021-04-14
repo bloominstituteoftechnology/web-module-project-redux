77e3b0b3bad0276c460553c6c8e8753006f32ada
nickcabuno <nickcabuno17@gmail.com>
2021-04-14 14:47:15 -0400

    car

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { reducer, initialState } from './reducers/reducer'
+
 
 const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+  
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className="boxes">
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './reducers/reducer'
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(reducer)
+console.log(store.getState());
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
