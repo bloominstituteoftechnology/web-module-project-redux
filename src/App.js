c61cb8d5916a325ad71341ab452b65766a46b083
Molly Brown <mollybrown0516@gmail.com>
2021-04-23 12:14:07 -0400

    Hooked up the power-lines with Provider and Store in index.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
+import { rootReducer } from "./reducer/reducer";//importing reducer from our reducer file
+import { createStore } from "redux"; //importing create store from redux here in App
 import { connect } from 'react-redux';//import the connect 
 import { reducer } from "./reducer/reducer";//import the reducer
 import { addFeature, removeFeature } from "./actions/action"; //Must import the explicit feature being exported from the file it lives in
+
+
+const store = createStore(rootReducer);
 
 const App = () => {
   
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from "react-redux"; //Connecting React to Redux here
+import { rootReducer } from "./reducer/reducer";//importing reducer from our reducer file
+import { createStore } from "redux"; //importing create store from redux here in App
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+//Setting up the store + provider --hooking up the power lines
+const store = createStore(rootReducer);
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, rootElement
+    );
