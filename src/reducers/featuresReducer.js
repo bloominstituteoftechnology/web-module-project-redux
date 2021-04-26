f15989b8d7b2b1d13193b0a91e2759071a11d093
yonathan Admasu <admyoni4@yahoo.com>
2021-04-25 23:41:34 -0700

    hit some bug still trying to fix outside office hours schedule

---
+++ src/reducers/featuresReducer.js
@@ -0,0 +1,24 @@
+const initialState = {
+    title: "Dragon Member List 🐲",
+    editing: false
+  };
+  
+  export const featuresReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case "ADD_FEATURE":
+        console.log("ADD_FEATURE action running in the feature reducer")
+        return {
+          ...state,
+          feature: action.payload
+          
+        };
+      case "REMOVE_FEATURE":
+        console.log("REMOVE_FEATURE action running in the feature reducer")
+        return {
+          ...state,
+          feature: state.feature !== action.payload
+        };
+      default:
+        return state;
+    }
+  };
