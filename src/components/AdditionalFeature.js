6b1b4870caa3afc29c0848182ff2d538200afc83
Lilia Ovcharenko <lilia.ovcharenko@gmail.com>
2021-04-25 23:36:33 -0700

    worked on the challenge still got some bugs

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,29 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
+import { connect } from "react-redux";
+import { featuresReducer } from '../reducers/featuresReducer';
+import { addFeature, removeFeature } from './../actions/updateFeatures';
 const AdditionalFeature = props => {
+  const initialState = {
+    feature: []
+  }
+  const [feature, dispatch] = useReducer(featuresReducer,initialState );
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button"  onClick= {() => dispatch(addFeature(feature))}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
-
-export default AdditionalFeature;
+const mapStateToProps = (state) => {
+  return {
+    feature: state.AdditionalFeature
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addFeature: feature => dispatch(addFeature(feature)),
+    removeFeature: feature => dispatch(removeFeature(feature))
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps )(AdditionalFeature);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 import 'bulma/css/bulma.css';
 import './styles.scss';
-
+const store = createStore(reducers, applyMiddleware(thunk));
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+      <App />
+</Provider>, 
+rootElement);
