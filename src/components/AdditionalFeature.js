e5f23c6df42779fb793ed559f4d00f37a9d41025
Kaseem Bradley <kaseembradley@Kaseems-Air.fios-router.home>
2021-05-11 18:27:54 -0400

    created action that adds feature price to total price

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
+import { addFeature } from "../actions/index";
+import { connect } from "react-redux";
 
-const AdditionalFeature = props => {
+const AdditionalFeature = (props) => {
+  const handleClick = (feature) => {
+    props.addFeature(feature);
+  };
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={() => handleClick(1000)}>
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(null, { addFeature })(AdditionalFeature);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { ADD_FEATURE } from "../actions/index";
+
 export const initialState = {
   additionalPrice: 0,
   car: {
@@ -17,11 +19,14 @@ export const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    // case ADD_FEATURE:
-    //   return {
-    //     ...state,
-    //     price: state.price,
-    //   };
+    case ADD_FEATURE:
+      return {
+        ...state,
+        car: {
+          ...state.car,
+          price: state.car.price + action.payload,
+        },
+      };
 
     default:
       return state;
