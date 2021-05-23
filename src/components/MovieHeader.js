327ff0ce7e1eb1e508557488b43a551bf73ac300
Craig Barcinas <cbarcinas@live.com>
2021-05-22 21:24:15 -0700

    connected redux

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,22 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
 const MovieHeader = (props) => {
-    const appTitle = "";
-    const displayFavorites = true;
-    
-    return(<div className="table-title">
-        <div className="row">
+  const appTitle = props.appTitle;
+  const displayFavorites = true;
+
+  return (
+    <div className="table-title">
+      <div className="row">
         <div className="col-sm-6">
-            <h2>{appTitle}</h2>
+          <h2>{appTitle}</h2>
         </div>
         <div className="col-sm-6 headerBar">
-            <div className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show"} Favorites</span></div>
-            <Link to="/movies" className="btn btn-sm btn-primary">View All Movies</Link>
-            <Link to="/movies/add" className="btn btn-sm btn-success"><i className="material-icons">&#xE147;</i> <span>Add New Movie</span></Link>
+          <div className="btn btn-sm btn-primary">
+            <span>{displayFavorites ? "Hide" : "Show"} Favorites</span>
+          </div>
+          <Link to="/movies" className="btn btn-sm btn-primary">
+            View All Movies
+          </Link>
+          <Link to="/movies/add" className="btn btn-sm btn-success">
+            <i className="material-icons">&#xE147;</i>{" "}
+            <span>Add New Movie</span>
+          </Link>
         </div>
-        </div>
-    </div>);
-}
+      </div>
+    </div>
+  );
+};
 
-export default MovieHeader;
+const mapStateToProps = (state) => {
+  return {
+    appTitle: state.appTitle,
+  };
+};
+
+export default connect(mapStateToProps)(MovieHeader);
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import MovieListItem from "./MovieListItem";
+import MovieFooter from "./MovieFooter";
 
-import MovieListItem from './MovieListItem';
-import MovieFooter from './MovieFooter';
+const MovieList = (props) => {
+  const movies = props.movies;
 
-const MovieList = (props)=> {
-    const movies = [];
+  return (
+    <div className="col">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Genre</th>
+            <th>Metascore</th>
+            <th></th>
+          </tr>
+        </thead>
 
-    return (
-        <div className="col">
-            <table className="table table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
-                    <th></th>
-                </tr>
-                </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <MovieListItem key={movie.id} movie={movie} />
+          ))}
+        </tbody>
+      </table>
 
-                <tbody>
-                    {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
-                    }
-                </tbody>
-            </table>
-            
-            <MovieFooter totalMovies={movies.length}/>
-        </div>
-    );
-}
+      <MovieFooter totalMovies={movies.length} />
+    </div>
+  );
+};
 
-export default MovieList;
+//   Map state to props
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+  };
+};
+
+export default connect(mapStateToProps)(MovieList);
