ba452a181abd5ab2f06894151acd03d3e362beed
SpacelessTime <73561672+SpacelessTime@users.noreply.github.com>
2021-05-24 00:11:31 -0400

    mvp

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,22 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {toggleFavorite} from '../actions/movieActions'
+const mapStatetoProps = state => {
+    return ({
+        appTitle: state.movie.appTitle,
+
+        displayFavorites: state.favorite.displayFavorites
+})
+}
 
 const MovieHeader = (props) => {
-    const appTitle = "";
-    const displayFavorites = true;
-    
+    const appTitle = props.appTitle;
+
+    //const displayFavorites = true;
+
+    const handleToggle = () => {
+        props.toggleFavorite(props.displayFavorites)
+}
     return(<div className="table-title">
+        
         <div className="row">
         <div className="col-sm-6">
+
             <h2>{appTitle}</h2>
         </div>
         <div className="col-sm-6 headerBar">
-            <div className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show"} Favorites</span></div>
+
+        <div className="btn btn-sm btn-primary"> <span onClick={handleToggle}> {props.displayFavorites ? "Hidden" : "Shown"} Favorites </span></div>
+
             <Link to="/movies" className="btn btn-sm btn-primary">View All Movies</Link>
             <Link to="/movies/add" className="btn btn-sm btn-success"><i className="material-icons">&#xE147;</i> <span>Add New Movie</span></Link>
-        </div>
-        </div>
-    </div>);
+</div>
+</div>
+</div>);
 }
 
-export default MovieHeader;
+//export default MovieHeader;
+export default connect(mapStatetoProps,{toggleFavorite})(MovieHeader)
