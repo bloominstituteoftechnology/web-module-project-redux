f2feee6f88cc29612e4ae6c53de6d74af204f2d7
Stan Tudor <stan2dor@gmail.com>
2021-05-24 11:56:10 -0700

    link App to redux

--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@ import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+
+const store = createStore(reducer);
+
 ReactDOM.render(
+  <Provider store={store}>
   <Router>
       <App />
   </Router>,
+  </Provider>,
   document.getElementById('root')
 );
