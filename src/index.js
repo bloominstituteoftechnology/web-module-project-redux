0b3c55d42ee06a39101e13beddc08c8e89ce5f23
John Vehmeyer <j.vehmeyer@gmail.com>
2021-06-08 10:53:37 -0500

    linked app to redux using createStore and Provider component

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import { createStore } from 'redux';
 import { Provider} from 'react-redux';
 
 import reducer from './reducers';
+// import movieReducer from './reducers';
+
 
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
