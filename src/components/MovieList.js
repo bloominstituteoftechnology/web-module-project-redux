447fe98a42356ab38a31af08ee38bea85e6464e9
ferengitooth <ehitsamill@gmail.com>
2021-07-13 22:30:45 -0700

    connected state into MovieList component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,10 @@
 import React from 'react';
-
+import { connect } from 'react-redux'
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
 
 const MovieList = (props)=> {
-    const movies = [];
-
+    const movies = props;
     return (
         <div className="col">
             <table className="table table-striped table-hover">
@@ -21,7 +20,7 @@ const MovieList = (props)=> {
 
                 <tbody>
                     {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
+                        props.movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
                     }
                 </tbody>
             </table>
@@ -31,4 +30,10 @@ const MovieList = (props)=> {
     );
 }
 
-export default MovieList;
+const mapStateToProps = (state) => {
+    return{
+        movies: state.movies
+    }
+}
+
+export default connect(mapStateToProps)(MovieList);
