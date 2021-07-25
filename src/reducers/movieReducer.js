f8dc25e4725524e7f45c11787d4b114555d32821
Angela Engel <amengel.ae@icloud.com>
2021-07-25 17:29:46 -0600

    worked on actions and reducers

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -10,8 +10,18 @@ const reducer = (state, action) => {
     switch(action.type) {
         case DELETE_MOVIE:
             return {
+                ...state,
                 movies: state.movies.filter(item=>(action.payload !== item.id))
             }
+        case ADD_MOVIE:
+            const newMovie = {
+                ...action.payload,
+                id: Date.now()
+            };
+            return {
+                ...state,
+                movies: [...state.movies, newMovie]
+                }
         default:
             return state;
     }
