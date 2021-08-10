0342b0bcdbcbe2c22075f1951690ac7b55403164
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-08-10 17:07:24 -0600

    Finished.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,34 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import MovieListItem from './MovieListItem';
-import MovieFooter from './MovieFooter';
+import MovieListItem from "./MovieListItem";
+import MovieFooter from "./MovieFooter";
+import { connect } from "react-redux";
 
-const MovieList = (props)=> {
-    const movies = [];
+const MovieList = (props) => {
+  return (
+    <div className="col">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Genre</th>
+            <th>Metascore</th>
+            <th></th>
+          </tr>
+        </thead>
 
-    return (
-        <div className="col">
-            <table className="table table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
-                    <th></th>
-                </tr>
-                </thead>
+        <tbody>
+          {props.movies.map((movie) => (
+            <MovieListItem key={movie.id} movie={movie} />
+          ))}
+        </tbody>
+      </table>
 
-                <tbody>
-                    {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
-                    }
-                </tbody>
-            </table>
-            
-            <MovieFooter totalMovies={movies.length}/>
-        </div>
-    );
-}
+      <MovieFooter totalMovies={props.movies.length} />
+    </div>
+  );
+};
 
-export default MovieList;
+const mapStateAsProps = (state) => {
+  console.log(state);
+  return {
+    movies: state.movies.movies,
+  };
+};
+
+export default connect(mapStateAsProps, {})(MovieList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,9 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import movieReducer from './movieReducer';
+import movieReducer from "./movieReducer";
+import favoritesReducer from "./favoritesReducer";
 
-export default movieReducer;
+export default combineReducers({
+  movies: movieReducer,
+  favorites: favoritesReducer,
+});
