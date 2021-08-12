94db140974d55a054c092fbae4199004eec480a3
Johanna Rodriguez <>
2021-08-11 19:02:33 -0500

    addMovie funtionallity done

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -3,3 +3,9 @@ export const DELETE_MOVIE = "DELETE_MOVIE";
 export const deleteMovie = (id)=>{
     return({type: DELETE_MOVIE, payload:id});
 }
+
+export const ADD_MOVIE = "ADD_MOVIE";
+
+export const addMovie = (movie)=>{
+    return({type: ADD_MOVIE, payload:movie});
+}
