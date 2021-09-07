27a83f0b1001fb7de40af88adb5b88a0a607e91b
Richard Oh <n24512@protonmail.com>
2021-09-07 17:21:39 -0500

    add movie function working

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -12,6 +12,11 @@ const reducer = (state = initialState, action) => {
       return {
         movies: state.movies.filter((item) => action.payload !== item.id),
       };
+    case ADD_MOVIE:
+      return {
+        ...state,
+        movies: [...state.movies, action.payload],
+      };
     default:
       return state;
   }
