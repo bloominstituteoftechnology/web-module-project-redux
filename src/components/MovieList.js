5f3ab242b25e30f8933d9188af5c96c43c3d9668
Richard Oh <n24512@protonmail.com>
2021-09-07 15:43:03 -0500

    connected the movie reducer, rendering movies

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
-import MovieListItem from './MovieListItem';
-import MovieFooter from './MovieFooter';
+import MovieListItem from "./MovieListItem";
+import MovieFooter from "./MovieFooter";
 
-const MovieList = (props)=> {
-    const movies = [];
+const MovieList = (props) => {
+  return (
+    <div className="col">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Genre</th>
+            <th>Metascore</th>
+            <th></th>
+          </tr>
+        </thead>
 
-    return (
-        <div className="col">
-            <table className="table table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
-                    <th></th>
-                </tr>
-                </thead>
+        <tbody>
+          {props.movies.map((movie) => (
+            <MovieListItem key={movie.id} movie={movie} />
+          ))}
+        </tbody>
+      </table>
 
-                <tbody>
-                    {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
-                    }
-                </tbody>
-            </table>
-            
-            <MovieFooter totalMovies={movies.length}/>
-        </div>
-    );
-}
+      <MovieFooter totalMovies={props.movies.length} />
+    </div>
+  );
+};
 
-export default MovieList;
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+  };
+};
+
+export default connect(mapStateToProps)(MovieList);
