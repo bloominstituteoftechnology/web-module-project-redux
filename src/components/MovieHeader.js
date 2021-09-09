6ac2662de04325288ab70524f1988acd71af73b1
courtneyharris <harriscourtney98@gmail.com>
2021-09-09 01:36:57 -0400

    Problems pushing

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+
 import { toggleFavorites } from './../actions/favoritesActions';
 
 const MovieHeader = (props) => {
