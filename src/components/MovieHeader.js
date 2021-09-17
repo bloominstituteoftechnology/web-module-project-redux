2389214fd3fe429ee47c0339ae9becabd21bbce3
Greg Brewton <gbrewton1980@gmail.com>
2021-09-17 13:07:18 -0600

    Connects movieReducer to MovieHeader

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const MovieHeader = (props) => {
-    const appTitle = "";
+    const appTitle = props.appTitle;
+    console.groupCollapsed('%cMovieHeader component', 'color: pink')
+    console.log('%cProps: ', 'color: yellow')
+    console.table(props)
+    console.groupEnd('MovieHeader component')
     const displayFavorites = true;
-    
-    return(<div className="table-title">
+
+    return (<div className="table-title">
         <div className="row">
-        <div className="col-sm-6">
-            <h2>{appTitle}</h2>
-        </div>
-        <div className="col-sm-6 headerBar">
-            <div className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show"} Favorites</span></div>
-            <Link to="/movies" className="btn btn-sm btn-primary">View All Movies</Link>
-            <Link to="/movies/add" className="btn btn-sm btn-success"><i className="material-icons">&#xE147;</i> <span>Add New Movie</span></Link>
-        </div>
+            <div className="col-sm-6">
+                <h2>{appTitle}</h2>
+            </div>
+            <div className="col-sm-6 headerBar">
+                <div className="btn btn-sm btn-primary"><span>{displayFavorites ? "Hide" : "Show"} Favorites</span></div>
+                <Link to="/movies" className="btn btn-sm btn-primary">View All Movies</Link>
+                <Link to="/movies/add" className="btn btn-sm btn-success"><i className="material-icons">&#xE147;</i> <span>Add New Movie</span></Link>
+            </div>
         </div>
     </div>);
 }
 
-export default MovieHeader;
+const mapStateToProps = state => {
+    return ({
+        appTitle: state.appTitle
+    })
+}
+
+export default connect(mapStateToProps)(MovieHeader);
