6df35673753050df768a2f60f912da0aad4fce2b
Ryan Mark <rymark907@gmail.com>
2021-10-05 14:17:21 -0700

    Able to add movies

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -10,7 +10,13 @@ const reducer = (state = initialState, action) => {
     switch(action.type) {
         case DELETE_MOVIE:
             return {
+                ...state,
                 movies: state.movies.filter(item=>(action.payload !== item.id))
+            }
+        case ADD_MOVIE:
+            return {
+                ...state,
+                movies: [...movies, {...action.payload}]
             }
         default:
             return state;
