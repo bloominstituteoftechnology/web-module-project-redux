ce99fd290d8ff4289d87a21c0eb95a1d6d33827a
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-10-05 23:40:47 -0700

    almost done

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
 import React from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
-import { connect } from 'react-redux';
+import { connect, Provider } from "react-redux";
+import { createStore } from "redux";
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieHeader from "./components/MovieHeader";
 
-import MovieHeader from './components/MovieHeader';
+import AddMovieForm from "./components/AddMovieForm";
+import FavoriteMovieList from "./components/FavoriteMovieList";
+import reducer from "./reducers/movieReducer";
+//create our store
+const store = createStore(reducer); //rootReducer will be a function with other reducers inside of it
 
-import AddMovieForm from './components/AddMovieForm';
-import FavoriteMovieList from './components/FavoriteMovieList';
-
-const App = props => {
+const App = (props) => {
   const displayFavorites = true;
 
   return (
-    <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/>Redux Module Project</span>
-      </nav>
+    <Provider store={store}>
+      <div>
+        <nav className="navbar navbar-dark bg-dark">
+          <span className="navbar-brand">
+            <img width="40px" alt="" src="./Lambda-Logo-Red.png" />
+            Redux Module Project
+          </span>
+        </nav>
 
-      <div className="container">
-        <MovieHeader/>
-        <div className="row ">
-          {displayFavorites && <FavoriteMovieList/>}
-        
-          <Switch>
-            <Route exact path="/movies/add">
-              <AddMovieForm />
-            </Route>
+        <div className="container">
+          <MovieHeader />
+          <div className="row ">
+            {displayFavorites && <FavoriteMovieList />}
 
-            <Route path="/movies/:id">
-              <Movie />
-            </Route>
+            <Switch>
+              <Route exact path="/movies/add">
+                <AddMovieForm />
+              </Route>
 
-            <Route path="/movies">
-              <MovieList/>
-            </Route>
+              <Route path="/movies/:id">
+                <Movie />
+              </Route>
 
-            <Route path="/">
-              <Redirect to="/movies"/>
-            </Route>
-          </Switch>
+              <Route path="/movies">
+                <MovieList />
+              </Route>
+
+              <Route path="/">
+                <Redirect to="/movies" />
+              </Route>
+            </Switch>
+          </div>
         </div>
       </div>
-    </div>
+    </Provider>
   );
 };
 
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,34 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import MovieListItem from './MovieListItem';
-import MovieFooter from './MovieFooter';
+import MovieListItem from "./MovieListItem";
+import MovieFooter from "./MovieFooter";
+import { connect } from "react-redux";
 
-const MovieList = (props)=> {
-    const movies = [];
+const MovieList = (props) => {
+  //   const movies = []; //we wouldn't be getting hard coded data from the app.
+  const { movies } = props;
 
-    return (
-        <div className="col">
-            <table className="table table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
-                    <th></th>
-                </tr>
-                </thead>
+  return (
+    <div className="col">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Genre</th>
+            <th>Metascore</th>
+            <th></th>
+          </tr>
+        </thead>
 
-                <tbody>
-                    {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
-                    }
-                </tbody>
-            </table>
-            
-            <MovieFooter totalMovies={movies.length}/>
-        </div>
-    );
-}
+        <tbody>
+          {movies.map((movie) => (
+            <MovieListItem key={movie.id} movie={movie} />
+          ))}
+        </tbody>
+      </table>
 
-export default MovieList;
+      <MovieFooter totalMovies={movies.length} />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies, //we need to map in the component
+  };
+};
+
+export default connect(mapStateToProps)(MovieList);
