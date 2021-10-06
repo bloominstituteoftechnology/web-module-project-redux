e10b672a7dae368a857a80bf231f0cb4e296b294
Ryan Mark <rymark907@gmail.com>
2021-10-06 08:57:21 -0700

    Removing favorited movies now works

--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -8,11 +8,8 @@ import { removeFavorites } from '../actions/favoriteActions';
 
 const FavoriteMovieList = (props) => {
 
-    const { id } = useParams();
-
-    const handleRemove = () => {
-        console.log(id);
-        props.dispatch(removeFavorites());
+    const handleRemove = (id) => {
+        props.dispatch(removeFavorites(id));
         
     }
     
@@ -23,7 +20,7 @@ const FavoriteMovieList = (props) => {
                 return <div key={movie.id}>
                     <Link className="btn btn-light savedButton" to={`/movies/${movie.id}`}>
                         {movie.title}
-                        <span><span class="material-icons" onClick={handleRemove}>remove_circle</span></span>
+                        <span><span class="material-icons" onClick={() => {handleRemove(movie.id)}}>remove_circle</span></span>
                     </Link> 
                 </div>
             })
