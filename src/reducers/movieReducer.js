b17aff04f0ae8460fe6eb996253e3bb9bb771292
scoutreilly <scouty33@gmail.com>
2021-10-17 23:57:36 -0600

    work in progress

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,20 +1,22 @@
-import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
-import movies from './../data.js';
+import { ADD_MOVIE, DELETE_MOVIE } from "../actions/movieActions.js";
+import movies from "./../data.js";
 
 const initialState = {
-    movies: movies,
-    appTitle: "IMDB Movie Database"
-}
+  favorites: [],
+  displayFavorites: true,
+};
 
-const reducer = (state, action) => {
-    switch(action.type) {
-        case DELETE_MOVIE:
-            return {
-                movies: state.movies.filter(item=>(action.payload !== item.id))
-            }
-        default:
-            return state;
-    }
-}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_MOVIE:
+      return {};
+    case DELETE_MOVIE:
+      return {
+        movies: state.movies.filter((item) => action.payload !== item.id),
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
