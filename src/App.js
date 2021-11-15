fe1481151d880313b9f019e71380a49f8b0ea539
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-11-15 11:02:50 -0500

    removed branding

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = props => {
   return (
     <div>
       <nav className="navbar navbar-dark bg-dark">
-        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/>Redux Module Project</span>
+        <span className="navbar-brand" >Redux Module Project</span>
       </nav>
 
       <div className="container">
