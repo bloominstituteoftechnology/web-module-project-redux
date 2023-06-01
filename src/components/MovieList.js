9202622c8d11ec2bf07aa574faf6d64eed9dfd11
Aaron Belmore <belmoreaaron9@gmail.com>
2023-06-01 19:25:15 -0400

    Connecting the Movie reducer step 2 complete

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,34 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import MovieListItem from './MovieListItem';
-import MovieFooter from './MovieFooter';
+import MovieListItem from "./MovieListItem";
+import MovieFooter from "./MovieFooter";
 
-const MovieList = (props)=> {
-    const movies = [];
+import { connect } from "react-redux";
 
-    return (
-        <div className="col">
-            <table className="table table-striped table-hover">
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
-                    <th></th>
-                </tr>
-                </thead>
+const MovieList = (props) => {
+  const movies = props.movies;
+  console.log(movies);
+  return (
+    <div className="col">
+      <table className="table table-striped table-hover">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Director</th>
+            <th>Genre</th>
+            <th>Metascore</th>
+            <th></th>
+          </tr>
+        </thead>
 
-                <tbody>
-                    {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
-                    }
-                </tbody>
-            </table>
-            
-            <MovieFooter totalMovies={movies.length}/>
-        </div>
-    );
-}
+        <tbody>
+          {movies.map((movie) => (
+            <MovieListItem key={movie.id} movie={movie} />
+          ))}
+        </tbody>
+      </table>
 
-export default MovieList;
+      <MovieFooter totalMovies={movies.length} />
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+  };
+};
+export default connect(mapStateToProps, {})(MovieList);
