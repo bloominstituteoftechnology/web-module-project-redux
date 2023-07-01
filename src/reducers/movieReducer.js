ec0ad368e692e7be7471c27821f754aaaee603ae
DJ1 <jimmynoble604@gmail.com>
2023-07-01 11:21:36 -0400

    moviesReducer.js

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -10,6 +10,7 @@ const reducer = (state = initialState, action) => {
     switch(action.type) {
         case DELETE_MOVIE:
             return {
+                ...state,
                 movies: state.movies.filter(item=>(action.payload !== item.id))
             }
         default:
